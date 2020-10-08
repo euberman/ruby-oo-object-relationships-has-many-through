88aaea843dabeaede3ac3fdd9c6b08c4ff03da9c
euberman <euberman@gmail.com>
2020-10-08 14:58:10 -0500

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
 
+    attr_accessor :name, :years
+    @@all
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal| meal.waiter == self }
+    end
+
+    def best_tipper
+        Meal.all.sort_by { |meal| meal.tip }.last.customer
+    end
+
 end
